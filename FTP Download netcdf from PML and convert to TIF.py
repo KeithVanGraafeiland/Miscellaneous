a118661f10dce899af1a7dcd842d5d10510298ee
KeithVanGraafeiland <kvangraafeiland@esri.com>
2021-09-18 14:58:56 +0000

    Created script for downloading chlorophyll-a from PML FTP and converting to TIF.

--- FTP Download netcdf from PML and convert to TIF.py
+++ FTP Download netcdf from PML and convert to TIF.py
@@ -1,20 +1,20 @@
 import ftplib
 import shutil
 import arcpy
+arcpy.env.overwriteOutput = True
 
 #Downloads Chlorophyll netCDF files from the Plymouth Marine Lab FTP site
 ftp = ftplib.FTP('ftp.rsg.pml.ac.uk')
 ftp.login(user='oc-cci-data', passwd = 'ELaiWai8ae')
 suburl = '/occci-v5.0/geographic/netcdf/monthly/chlor_a/'
-yeardownload = '2020'
+yeardownload = '2000'
 ftp.cwd(suburl + yeardownload)
 filenames = ftp.nlst()
-fileloc = 'E:/ChlorA/PML/5.0'
+fileloc = 'D:/ChlorA/PML/5.0'
 #example fullurl = 'ftp://ftp.rsg.pml.ac.uk/occci-v4.2/geographic/netcdf/monthly/chlor_a/'
-arcpy.env.workspace = 'E:/ChlorA/PML/5.0/TIF'
+arcpy.env.workspace = 'D:/ChlorA/PML/5.0/TIF'
 
 for filename in filenames:
-
     with open( filename, 'wb' ) as file :
         print ("Downloading file .......... ", filename)
         ftp.retrbinary('RETR %s' % filename, file.write, 1024)
@@ -25,5 +25,5 @@ for filename in filenames:
         print("Processing: " + filename)
         arcpy.md.MakeNetCDFRasterLayer(fileloc + "\\" + filename, "chlor_a", "lon", "lat", ras, '', '', "BY_VALUE", "CENTER")
         arcpy.Raster(ras).save("chlor_a_PML_v5_0_" + filename.split("-")[-2] + ".tif")
-print("Done Downloading!")
+print("Done Downloading and Processing " + yeardownload)
 ftp.quit()
